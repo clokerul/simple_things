90f1d353ea0faaa997a4f19a2a7d9a60725e5251
George <lazureanugeorge@yahoo.com>
2023-04-20 09:30:02 +0300

    Added post method and refactored server

--- Backend/api/src/server.py
+++ Backend/api/src/server.py
@@ -1,31 +1,15 @@
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+import sys, os
+sys.path.append(os.path.join(sys.path[0],'controllers'))
+sys.path.append(os.path.join(sys.path[0],'db'))
+from quotes import quotes_page
 
-quotes = [
-    {
-        "id": 1,
-        "quote": "Your mind will lead you to glorious places, but you have to let it.",
-        "author": "unkown",
-        "isRegret": False,
-        "hits": 12345,
-    },
-    {
-        "id": 2,
-        "quote": "To offer respect is free, you cannot know the future of the person you are speaking to right now and how" +\
-            " it will affect yours.",
-        "author": "unkown",
-        "isRegret": False,
-        "hits": 4123
-    },
-    {
-        "id": 3,
-        "quote": "Falling in love is like playing russian rullete with your heart, be very very carefull.",
-        "author": "Mircea Tanase",
-        "isRegret": True,
-        "hits": 5123
-    },
-]
+app = Flask(__name__)
+app.register_blueprint(quotes_page)
+
+def filterDataBy(filterFunction, data):
+    return jsonify(list(filter(filterFunction, data)))
 
 users = [
     {
@@ -36,16 +20,9 @@ users = [
     }
 ]
 
-# Todo change to db call
-def fetchQuotes():
-    return quotes
-
 def fetchUsers():
     return users
-
-def filterDataBy(filterFunction, data):
-    return jsonify(list(filter(filterFunction, data)))
-
+### USERS
 @app.route('/users')
 def getUsers():
     return jsonify(users)
@@ -54,6 +31,7 @@ def getUsers():
 def getUsersByusername(username):
     return filterDataBy(lambda user : user["username"] == username, fetchUsers())
 
+### LOGIN
 @app.route('/login/<username>/<password>')
 def postLogin(username, password):
     user = list(filter(lambda user : user["username"] == username, fetchUsers()))[0]
@@ -61,18 +39,6 @@ def postLogin(username, password):
         return f'{user["cookie"]}', 200
     else:
         return 'Denied', 401
-    
-@app.route('/quotes')
-def getQuotes():
-    return jsonify(fetchQuotes())
-
-@app.route('/pathways')
-def getPathways():
-    return filterDataBy(lambda quote : not quote["isRegret"], fetchQuotes())
-
-@app.route('/regrets')
-def getRegrets():
-    return filterDataBy(lambda quote : quote["isRegret"], fetchQuotes())
 
 @app.route('/')
 def hello():
